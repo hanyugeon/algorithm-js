4b27a1f677fb18a08b8ce80d61983167e4c95027
hanyugeon <hanyugeon@naver.com>
2022-03-10 21:17:19 +0900

    monthly01/006 조건에 맞게 좌표를 움직여 숫자 넣기(복습 필요)

---
+++ monthly_challenge_01/006.js
@@ -0,0 +1,31 @@
+// 삼각 달팽이(좌표 찍기)
+// 나의 풀이
+function solution(n) {
+  const answer = [];
+  let snail = [];
+
+  for (let i = 1; i < n + 1; i++) {
+    const temp = Array(i).fill(0);
+    snail.push(temp);
+  }
+
+  
+
+
+
+  return ;
+}
+
+/**
+ * fill()
+ * map()
+ * flatMap()
+ * concat()
+ */
+
+/**
+ * 좌표 찍기.
+ * 좌표를 어떠한 기준으로 옮겨갈것인가?
+ * 좌표를 옮긴다음 해당 숫자 넣기
+ * 모든 숫자가 조건에 맞게 들어간 이후에 하나의 배열로 반환.
+ */
