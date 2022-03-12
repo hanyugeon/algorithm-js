c5aefe0f17e618db7e3d3fbf77bb37e115d75d53
hanyugeon <hanyugeon@naver.com>
2022-03-12 20:55:54 +0900

    monthly02/003 올바른 조건속에서 진수 비교를 통한 요구사항 해결(다른 풀이)

--- monthly_challenge_02/003.js
+++ monthly_challenge_02/003.js
@@ -34,3 +34,34 @@ function solution(numbers) {
 
   return answer;
 }
+
+// 다른 풀이
+function solution(numbers) {
+  const answer = [];
+
+  numbers.forEach(e => {
+    let binNum = '0' + e.toString(2);
+    let binLen = binNum.length;
+    
+    if (binNum[binLen - 1] === '0') {
+      answer.push(e + 1);
+    } else {
+      for (let i = binLen; i >= 0; i--) {
+        if (binNum[i] === '0') {
+          answer.push(parseInt(binNum.substring(0, i) + "1" + "0" + binNum.substring(i + 2, l), 2));          
+          break;
+        }
+      }
+    }
+  });
+
+  return answer;
+}
+
+/**
+ * 1. 맨 끝자리가 0인 경우
+ * 2. 01이 나오는 경우 => 01을 서로 10으로 바꾸어 준다.
+ * 활용한 메서드
+ * forEach()
+ * 문자열 특정 위치 바꾸어서 출력하기
+ */
