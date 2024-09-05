70a6caa7c98418c7861c91d912a442040e58710f
hanyugeon <hanyugeon@naver.com>
2024-09-05 15:54:58 +0900

    프로그래머스: 올바른 괄호 (LV2) - 스택

--- level02/010.js
+++ level02/010.js
@@ -1,31 +1,29 @@
-// 올바른 괄호
+/**
+ * 올바른 괄호 (스택 Stack)
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12909
+ */
 
-/*
-# 문제 설명
-괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다.
+function solution(s) {
+  if (s[0] === ")") return false;
+  if (s.length % 2 !== 0) return false;
+  if (s[s.length - 1] === "(") return false;
 
-# 예를 들어
-"()()" 또는 "(())()" 는 올바른 괄호입니다.
-")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
-'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 
-문자열 s가 올바른 괄호이면 true를 return 하고, 
-올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+  let stackLength = 0;
 
-# 제한사항
-문자열 s의 길이 : 100,000 이하의 자연수
-문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+  for (let idx = 0; idx < s.length; idx += 1) {
+    if (s[idx] === "(") {
+      stackLength += 1;
+    } else if (stackLength === 0) {
+      return false;
+    } else {
+      stackLength -= 1;
+    }
+  }
 
-# 입출력 예
-s	answer
-"()()"	true
-"(())()"	true
-")()("	false
-"(()("	false
-입출력 예 설명
-입출력 예 #1,2,3,4
-문제의 예시와 같습니다.
-*/
+  return stackLength === 0 ? true : false;
+}
 
+// 이전 풀이
 // 나의풀이
 /*
   내가 생각한 구조
@@ -36,22 +34,22 @@ s	answer
   03. 반복문 이후 count가 0이 아닐 경우 -> false 반환.
 */
 
-function solution(s) {
-  let count = 0;
+// function solution(s) {
+//   let count = 0;
 
-  if (s.length % 2 != 0) return false;
+//   if (s.length % 2 != 0) return false;
 
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(") {
-      count++;
-    } else if (s[i] === ")") {
-      count--;
-    }
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === "(") {
+//       count++;
+//     } else if (s[i] === ")") {
+//       count--;
+//     }
 
-    if (count < 0) return false;
-  }
+//     if (count < 0) return false;
+//   }
 
-  if (count != 0) return false;
+//   if (count != 0) return false;
 
-  return true;
-}
+//   return true;
+// }
