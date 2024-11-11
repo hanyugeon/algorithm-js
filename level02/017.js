5be6da68d6a9c8f49215aa8acc0bc551ca18fd45
hanyugeon <hanyugeon@naver.com>
2024-11-12 02:01:47 +0900

    프로그래머스: 게임 맵 최단거리 (LV2) - BFS (Queue 활용, 재귀함수 X)

--- level02/017.js
+++ level02/017.js
@@ -1,48 +1,18 @@
-// function solution(maps) {
-//   let result = 0;
+/**
+ * 게임 맵 최단거리
+ * https://school.programmers.co.kr/learn/courses/30/lessons/1844
+ */
 
-//   const n = maps.length;
-//   const m = maps[0].length;
-//   const visited = new Array(n).fill().map((val) => new Array(m).fill(0));
+/**
+ * BFS일지 DFS일지 판단하기
+ * 해당 문제는 BFS
+ *
+ * queue 활용이 진짜 근본인듯
+ * 재귀함수로 삽질하지말고
+ * queue를 활용할 수 있도록 노력해보자
+ */
 
-//   const dx = [1, 0, -1, 0];
-//   const dy = [0, 1, 0, -1];
-
-//   const queue = [];
-//   queue.push([0, 0]);
-
-//   visited[[0][0]] = 1;
-
-//   if (maps[n - 2][m - 1] === 0 && maps[n - 1][m - 2] === 0) {
-//     return -1;
-//   }
-
-//   while (queue.length > 0) {
-//     const [y, x] = queue.shift();
-
-//     for (let i = 0; i < 4; i++) {
-//       let ny = y + dy[i];
-//       let nx = x + dx[i];
-
-//       if (ny < 0 || nx < 0 || ny >= n || nx >= m || maps[ny][nx] === 0) {
-//         continue;
-//       }
-//       if (visited[ny][nx]) {
-//         continue;
-//       }
-
-//       queue.push([ny, nx]);
-//       visited[ny][nx] = visited[y][x] + 1;
-//     }
-//   }
-
-//   result = visited[n - 1][m - 1];
-
-//   if (!result) return -1;
-
-//   return result;
-// }
-
+// 24.08.29 풀이
 function solution(maps) {
   const n = maps.length;
   const m = maps[0].length;
@@ -82,4 +52,41 @@ function solution(maps) {
 
   bfs(0, 0);
   return visited[n - 1][m - 1] ? visited[n - 1][m - 1] : -1;
+}
+
+// 24.11.12 풀이
+function solution(maps) {
+  const n = maps.length;
+  const m = maps[0].length;
+  const isVisited = Array.from(Array(n), () => Array(m).fill(null));
+
+  const dx = [1, 0, -1, 0];
+  const dy = [0, 1, 0, -1];
+
+  const bfs = (x, y) => {
+    const queue = [[x, y]];
+    isVisited[x][y] = 1;
+
+    while (queue.length) {
+      const [currentX, currentY] = queue.shift();
+
+      for (let d = 0; d < dx.length; d += 1) {
+        const newX = currentX + dx[d];
+        const newY = currentY + dy[d];
+
+        if (newX < 0 || newX > n - 1) continue;
+        if (newY < 0 || newY > m - 1) continue;
+        if (maps[newX][newY] === 0) continue;
+        if (isVisited[newX][newY] !== null) continue;
+
+        const prevDepth = isVisited[currentX][currentY];
+        queue.push([newX, newY]);
+        isVisited[newX][newY] = prevDepth + 1;
+      }
+    }
+  };
+
+  bfs(0, 0);
+
+  return isVisited[n - 1][m - 1] || -1;
 }
