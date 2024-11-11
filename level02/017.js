bc8820d695f0ff320376436599655779c3e743bf
hanyugeon <hanyugeon@naver.com>
2024-11-12 02:08:49 +0900

    프로그래머스: 게임 맵 최단거리 (LV2) - BFS 효율성 향상

--- level02/017.js
+++ level02/017.js
@@ -16,52 +16,16 @@
 function solution(maps) {
   const n = maps.length;
   const m = maps[0].length;
-
-  const visited = new Array(n).fill().map((val) => new Array(m).fill(0));
-
-  const dx = [0, 1, 0, -1];
-  const dy = [1, 0, -1, 0];
-
-  if (maps[n - 1][m - 2] === 0 && maps[n - 2][m - 1] === 0) return -1;
-
-  const bfs = (x, y) => {
-    const queue = [];
-
-    visited[x][y] = 1;
-    queue.push([x, y]);
-
-    while (queue.length) {
-      const [cx, cy] = queue.shift();
-
-      for (let dir = 0; dir < 4; dir += 1) {
-        const nx = cx + dx[dir];
-        const ny = cy + dy[dir];
-
-        if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-          continue;
-        }
-        if (maps[nx][ny] === 0 || visited[nx][ny] !== 0) {
-          continue;
-        }
-
-        visited[nx][ny] = visited[cx][cy] + 1;
-        queue.push([nx, ny]);
-      }
-    }
-  };
-
-  bfs(0, 0);
-  return visited[n - 1][m - 1] ? visited[n - 1][m - 1] : -1;
-}
-
-// 24.11.12 풀이
-function solution(maps) {
-  const n = maps.length;
-  const m = maps[0].length;
   const isVisited = Array.from(Array(n), () => Array(m).fill(null));
 
   const dx = [1, 0, -1, 0];
   const dy = [0, 1, 0, -1];
+
+  if (n === 1 && m === 2) return 2;
+  if (n === 2 && m === 1) return 2;
+  if (n > 1 && m > 1) {
+    if (maps[n - 2][m - 1] === 0 && maps[n - 1][m - 2] === 0) return -1;
+  }
 
   const bfs = (x, y) => {
     const queue = [[x, y]];
