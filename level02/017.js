ba6b0d0e3d97ffa25094c1e1f08aa1bc3fc61d0d
hanyugeon <hanyugeon@naver.com>
2024-11-13 18:36:32 +0900

    프로그래머스: 게임 맵 최단거리 (LV2) - BFS (복습)

--- level02/017.js
+++ level02/017.js
@@ -10,9 +10,56 @@
  * queue 활용이 진짜 근본인듯
  * 재귀함수로 삽질하지말고
  * queue를 활용할 수 있도록 노력해보자
+ *
+ * graph와 queue 선언
+ * queue에 시작 node 삽입
+ * deQueue된 node에서 갈 수 있는 노드 하나씩 inQueue
+ * 이때 방문한 node는 체크해서 사이클을 방지 할 것!
  */
 
 // 24.08.29 풀이
+function solution(maps) {
+  const n = maps.length;
+  const m = maps[0].length;
+
+  const visited = new Array(n).fill().map((val) => new Array(m).fill(0));
+
+  const dx = [0, 1, 0, -1];
+  const dy = [1, 0, -1, 0];
+
+  if (maps[n - 1][m - 2] === 0 && maps[n - 2][m - 1] === 0) return -1;
+
+  const bfs = (x, y) => {
+    const queue = [];
+
+    visited[x][y] = 1;
+    queue.push([x, y]);
+
+    while (queue.length) {
+      const [cx, cy] = queue.shift();
+
+      for (let dir = 0; dir < 4; dir += 1) {
+        const nx = cx + dx[dir];
+        const ny = cy + dy[dir];
+
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+          continue;
+        }
+        if (maps[nx][ny] === 0 || visited[nx][ny] !== 0) {
+          continue;
+        }
+
+        visited[nx][ny] = visited[cx][cy] + 1;
+        queue.push([nx, ny]);
+      }
+    }
+  };
+
+  bfs(0, 0);
+  return visited[n - 1][m - 1] ? visited[n - 1][m - 1] : -1;
+}
+
+// 24.11.12 풀이
 function solution(maps) {
   const n = maps.length;
   const m = maps[0].length;
